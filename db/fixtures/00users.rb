71260191bf57af7aba7edb2f947614e83b021ba7
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-10-25 23:01:26 +0100

    working password - salt for users seed data

--- db/fixtures/00users.rb
+++ db/fixtures/00users.rb
@@ -5,7 +5,7 @@ User.seed(:login) do |u|
   u.login = "mark.odermatt"
   u.email = "mark_odermatt@bluewin.ch"
   u.is_admin = true
-  u.hashed_password = "34242cde00981d1beb333d1c99c069e2ce865d56"
+  u.hashed_password = User.encrypted_password('secret', "aykso278sdfeiyxiu")
   u.salt = "aykso278sdfeiyxiu"
 end
 
@@ -16,7 +16,7 @@ User.seed(:login) do |u|
   u.login = "patrick.buesser"
   u.email = "p.buesser@luetze.ch"
   u.is_admin = true
-  u.hashed_password = "34242cde00981d1beb333d1c99c069e2ce865d56"
+  u.hashed_password = User.encrypted_password('secret', "yloi4645aelyl")
   u.salt = "yloi4645aelyl"
 end
 
@@ -27,7 +27,7 @@ User.seed(:login) do |u|
   u.login = "marcel.franze"
   u.email = "marcel.franze@greenmail.ch"
   u.is_admin = true
-  u.hashed_password = "34242cde00981d1beb333d1c99c069e2ce865d56"
+  u.hashed_password = User.encrypted_password('secret', "yko894ajoilkakjyxz")
   u.salt = "yko894ajoilkakjyxz"
 end
 
@@ -37,7 +37,7 @@ User.seed(:login) do |u|
   u.firstname = "Roman"
   u.login = "roman.maechler"
   u.email = "r.maechler@rkb.ch"
-  u.hashed_password = "34242cde00981d1beb333d1c99c069e2ce865d56"
+  u.hashed_password = User.encrypted_password('secret', "yueoiyxe45wilyo")
   u.salt = "yueoiyxe45wilyo"
 end
 
@@ -46,7 +46,7 @@ User.seed(:login) do |u|
   u.lastname = "Maechler"
   u.firstname = "Pascal"
   u.login = "pascal.maechler"
-  u.hashed_password = "34242cde00981d1beb333d1c99c069e2ce865d56"
+  u.hashed_password = User.encrypted_password('secret', "oilalo4wyla4kl")
   u.salt = "oilalo4wyla4kl"
 end
 
@@ -56,6 +56,6 @@ User.seed(:login) do |u|
   u.firstname = "Michael"
   u.login = "michael.janser"
   u.email = "m_janser@gmx.ch"
-  u.hashed_password = "34242cde00981d1beb333d1c99c069e2ce865d56"
+  u.hashed_password = User.encrypted_password('secret', "5as85d5g")
   u.salt = "5as85d5g"
 end
