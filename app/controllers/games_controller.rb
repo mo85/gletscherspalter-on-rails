ff183def0d5724e54b730e39a542ff7b6c251d3e
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-10-31 00:37:15 +0100

    removed unneeded code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,18 +3,15 @@ class GamesController < ApplicationController
   filter_access_to :all
   
   # GET /games
-  # GET /games.xml
   def index
     @title = "Gletscherspatler.ch::Spiele"
     @games = current_season.games
     respond_to do |format|
-      format.html # insufficientcredentials.html.erb
-      format.xml  { render :xml => @games }
+      format.html
     end
   end
 
   # GET /games/1
-  # GET /games/1.xml
   def show
     @game = Game.find(params[:id])
     
@@ -22,18 +19,15 @@ class GamesController < ApplicationController
     
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @game }
     end
   end
 
   # GET /games/new
-  # GET /games/new.xml
   def new
     @game = Game.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @game }
     end
   end
 
@@ -43,7 +37,6 @@ class GamesController < ApplicationController
   end
 
   # POST /games
-  # POST /games.xml
   def create
     @game = Game.new(params[:game])
 
@@ -52,40 +45,33 @@ class GamesController < ApplicationController
         current_season.games << @game
         flash[:notice] = 'Game was successfully created.'
         format.html { redirect_to(@game) }
-        format.xml  { render :xml => @game, :status => :created, :location => @game }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @game.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /games/1
-  # PUT /games/1.xml
   def update
     @game = Game.find(params[:id])
 
     respond_to do |format|
       if @game.update_attributes(params[:game])
         flash[:notice] = 'Game was successfully updated.'
-        format.html { redirect_to(@game) }
-        format.xml  { head :ok }
+        format.html { redirect_to(games_path) }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @game.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /games/1
-  # DELETE /games/1.xml
   def destroy
     @game = Game.find(params[:id])
     @game.destroy
 
     respond_to do |format|
       format.html { redirect_to(games_url) }
-      format.xml  { head :ok }
     end
   end
 end
