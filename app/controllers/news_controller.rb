61822eba13265729f01fb3b30e1d9df9def5968b
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-11-18 13:00:02 +0100

    news contd

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -25,22 +25,43 @@ class NewsController < ApplicationController
   end
   
   def create
-    @news = News.new
+    @news = News.new(params[:news])
+    @news.publisher = current_user
     
     respond_to do |format|
-      format.html
+      if @news.save
+        flash[:notice] = "News Eintrag erstellt."
+        format.html { redirect_to news_index_path }
+      else
+        format.html { render :action => "new" } 
+      end
     end
   end
   
   def edit
-    
+    @news = News.find(params[:id])
   end
   
   def update
+    @news = News.find(params[:id])
     
+    respond_to do |format|
+      if @news.update_attributes(params[:news])
+        format.html { redirect_to news_index_path }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
   end
   
-  def destroy
+  def destroy  
+    @news = News.find(params[:id])
+    @news.destroy
+
+    respond_to do |format|
+      flash[:notice] = "News Eintrag gelöscht."
+      format.html { redirect_to(news_index_path) }
+    end
   end
   
 end
