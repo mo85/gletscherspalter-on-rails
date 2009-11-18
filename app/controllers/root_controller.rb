b236d5aebcf27349ae1bb3fa5edeaff68d417f05
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-11-18 15:32:33 +0100

    news on top page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,7 +4,7 @@ class RootController < ApplicationController
 
   def index
     @title = "Gletscherspalter.ch::Home"
-    @news = Message.all :limit => 2
+    @news = News.find :all, :limit => 2, :order => "created_at DESC"
     
     @games = current_season.games.future_games :limit => 3
     @events = Event.future_events(:order => "date DESC", :limit => 3)
