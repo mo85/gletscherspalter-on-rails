eab0c7a213ad97169d194ea7ba44c8eaaa38f5a8
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-11-19 18:42:25 +0100

    refactoring & larger images

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,8 +1,6 @@
 module PhotosHelper
   
-  #
   # Returns a flickr image tag for the given photo
-  # 
   # size and lightbox paramater could be
   # 
   # :small => 75x75
@@ -10,7 +8,6 @@ module PhotosHelper
   # :medium => 240px width
   # :large => 500px width
   # :original => original width and height
-  #
   def photo_image_tag(photo, options)
     link_to(
       image_tag(
@@ -20,17 +17,14 @@ module PhotosHelper
         { :rel => 'lightbox[roadtrip]'})
   end
   
-  #
   # Returns a lightboxed flickr image tag for the given photoset
   # 
   # size paramater could be
-  # 
   # :small => 75x75
   # :thumbnail => 100px width
   # :medium => 240px width
   # :large => 500px width
   # :original => original width and height
-  #
   def photoset_image_tag(photo, options)
     image_tag(
       get_url(photo, photo.primary, options[:size]),
@@ -39,36 +33,29 @@ module PhotosHelper
   
   private 
 
-  #
-  # generates the flickr url
-  #
+  # generate the flickr url
   def get_url(photo, identifier, size)
-    sprintf("http://farm%s.static.flickr.com/%s/%s_%s%s.jpg", 
-      photo.farm.to_s, photo.server, identifier, photo.secret, 
-        get_url_size(size))
+    "http://farm#{photo.farm.to_s}.static.flickr.com/#{photo.server}/#{
+      identifier}_#{photo.secret}#{get_url_size(size)}.jpg"
   end
 
-  #
   # gets the size identifier for the url
-  #
   def get_url_size(size)
     case size  
       when :small
-        "_s"
+        "_m"
       when :thumbnail
         "_t"
       when :medium
-        "_m"
-      when :large
         ""
+      when :large
+        "_b"
       when :original
         "_o"
     end
   end
   
-  #
   # Returns the image tag options depending on the choosen size
-  #
   def get_options(photo, size)
     options = { :alt => photo.title, :class => 'bevel' }
     case size  
