58786585c3c58d39382c77e7b84fcdbf307610a8
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-11-19 15:43:55 +0100

    gallery with bad layout working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,16 +2,19 @@ class PhotosController < ApplicationController
   
   include FlickRaw
   
-  ::FLICKR_API_KEY = "02c75fd7a65f0e72affa799b7105afc9"
-  ::SHARED_SECRET = "3125e4dc1dd61ad4"
+  FlickRaw.api_key = "02c75fd7a65f0e72affa799b7105afc9"
+  FlickRaw.shared_secret = "3125e4dc1dd61ad4"
   ::USER_ID = "44777339@N04"
+  
+  @auth = flickr.auth.checkToken :auth_token => "72157622835493126-87c74181a6135d00"
   
   def index
     @photosets = flickr.photosets.getList :user_id => USER_ID
   end
   
-  def show
-    
+  def show  
+    @photos = flickr.photosets.getPhotos(:photoset_id => params[:id]).photo
+    @photoset = flickr.photosets.getInfo(:photoset_id => params[:id])
   end
   
 end
