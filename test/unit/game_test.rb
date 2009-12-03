a9f73d332a64bef73a07799ec476245c1ae2e3be
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-12-03 16:14:31 +0100

    tests for season stats added

--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
   
-  test "the truth" do
-    assert true
-  end
-
+  fixtures :games
+  
+  # test "last game should be against Dukla (4:4)" do
+  #   assert_equal Game.last_game, games(:dukla) 
+  # end
+  # 
+  # test "next game should be against Sagmehlfeger" do
+  #   assert_equal Game.next_game, games(:sagmehlfeger)
+  # end
+  
 end
--- test/unit/season_test.rb
+++ test/unit/season_test.rb
@@ -1,8 +1,50 @@
 require 'test_helper'
 
 class SeasonTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  fixtures :seasons
+  fixtures :games
+  
+  def setup
+    @previous_season = seasons(:previous)
+    @current_season = seasons(:actual)
+    @next_season = seasons(:next)
+  end
+  
+  
+  test "games played" do
+    assert_equal 2, @previous_season.games_played.size
+    assert_equal 4, @current_season.games_played.size
+    assert_equal 0, @next_season.games_played.size 
+  end
+  
+  test "won games" do
+    assert_equal 1, @previous_season.wins.size
+    assert_equal 2, @current_season.wins.size
+    assert_equal 0, @next_season.wins.size
+  end
+  
+  test "tied games" do
+    assert_equal 0, @previous_season.tied_games.size
+    assert_equal 1, @current_season.tied_games.size
+    assert_equal 0, @next_season.tied_games.size
+  end
+  
+  test "games that we lost" do
+    assert_equal 1, @previous_season.defeats.size
+    assert_equal 1, @current_season.defeats.size
+    assert_equal 0, @next_season.defeats.size
+  end
+  
+  test "scored goals" do
+    assert_equal 11, @previous_season.scored_goals
+    assert_equal 12, @current_season.scored_goals
+    assert_equal 0, @next_season.scored_goals
+  end
+  
+  test "goals against" do
+    assert_equal 9, @previous_season.goals_against
+    assert_equal 14, @current_season.goals_against
+    assert_equal 0, @next_season.goals_against
   end
 end
