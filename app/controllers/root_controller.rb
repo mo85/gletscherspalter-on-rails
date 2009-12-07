1064c9f52dc7f8383d14c5545b2d319cf52ba23e
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-12-07 14:36:50 +0100

    bla

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,8 +4,8 @@ class RootController < ApplicationController
   
   ::GMAPS_API_KEY = "ABQIAAAA5HvxKEk6euzB7DUyH0_WQRTpKOENAK7M60P_iZyjsCmeWfdXDBRvBLY2QxpZG45FwPCzYLQvO4CdXw"
   
-  page_cache :contact
-  page_cache :locations
+  caches_page :contact
+  caches_page :locations
   
   def index
     @title = "Gletscherspalter.ch::Home"
