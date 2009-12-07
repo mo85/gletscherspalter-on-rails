5bffb7688cd4c138b22125ee0b7e967637124704
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-12-07 01:40:54 +0100

    remaining side bar changes

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -26,11 +26,12 @@ authorization do
   
   role :admin do
     includes :user
-    has_permission_on [:players, :users, :scores, :topics, :news, :guestnotes], :to => [:read, :manage]
+    has_permission_on [:players, :users, :scores, :topics, :news, :guestnotes, :photos], :to => [:read, :manage]
     has_permission_on :players, :to => :subscribe_to_games
     has_permission_on [:locations, :events, :messages], :to => [:read, :manage]
     has_permission_on :games, :to => [:read, :manage, :add_or_remove_players_from_games]
     has_permission_on :posts, :to => :update_posts
+
   end
 end
 
