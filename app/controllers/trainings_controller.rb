48a428c37acb9ff2c72a91b29b0cfef750d68726
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-12-09 23:21:14 +0100

    missing files

---
+++ app/controllers/trainings_controller.rb
@@ -0,0 +1,34 @@
+class TrainingsController < ApplicationController
+  
+  def new
+    @training = "hihi"
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def create
+    @training = "hihi"
+    
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def edit
+    @training = "hihi"
+    
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def update
+    @training = "hihi"
+  end
+  
+  def destroy
+  end
+  
+  
+end
---
+++ app/controllers/trainingscamps_controller.rb
@@ -0,0 +1,34 @@
+class TrainingscampsController < ApplicationController
+  
+  def new
+    @trainingscamp = "hihi"
+    
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def create
+    @trainingscamp = "hihi"
+    
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def edit
+    @trainingscamp = "hihi"
+    
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def update
+    @trainingscamp = "hihi"
+  end
+  
+  def destroy
+  end
+  
+end
