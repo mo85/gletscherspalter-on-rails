ced72ae66dfc860ed12d32418c62862d5b6fa421
Mark Odermatt <mark_odermatt@bluewin.ch>
2009-12-09 15:23:31 +0100

    local timezone for games time

--- db/fixtures/06games.rb
+++ db/fixtures/06games.rb
@@ -1,5 +1,5 @@
 Game.seed(:date, :opponent) do |s|
-  s.date = Time.parse("2009/10/18 - 08:10")
+  s.date = Time.local(2009, "Oct",18, 8, 10)
   s.opponent = "Albula Devils"
   s.score = 11
   s.opponent_score = 6
@@ -8,7 +8,7 @@ Game.seed(:date, :opponent) do |s|
 end
 
 Game.seed(:date, :opponent) do |s|
-  s.date = Time.parse("2009/10/25 - 08:30")
+  s.date = Time.local(2009, "Oct", 25, 8,30)
   s.opponent = "Uusrutscher"
   s.score = 2
   s.opponent_score = 6
@@ -17,42 +17,42 @@ Game.seed(:date, :opponent) do |s|
 end
 
 Game.seed(:date, :opponent) do |s| 
-  s.date = Time.parse("2009/11/08 - 08:15")
+  s.date = Time.local(2009, "Nov", 8, 8,15)
   s.opponent = "Dukla VBZ"
   s.rink_id = 2
   s.season_id = 1
 end
 
 Game.seed(:date, :opponent) do |s| 
-  s.date = Time.parse("2009/11/14 - 09:45")
+  s.date = Time.local(2009, "Nov", 14, 9,45)
   s.opponent = "Bone Crashers"
   s.rink_id = 6
   s.season_id = 1
 end
 
 Game.seed(:date, :opponent) do |s| 
-  s.date = Time.parse("2009/11/21 - 09:45")
+  s.date = Time.local(2009, "Nov", 21, 9, 45)
   s.opponent = "Albula Devils"
   s.rink_id = 6
   s.season_id = 1
 end
 
 Game.seed(:date, :opponent) do |s| 
-  s.date = Time.parse("2009/11/26 - 21:30")
+  s.date = Time.local(2009, "Nov", 26, 21, 30)
   s.opponent = "Bone Crashers"
   s.rink_id = 8
   s.season_id = 1
 end
 
 Game.seed(:date, :opponent) do |s| 
-  s.date = Time.parse("2009/11/28 - 21:15")
+  s.date = Time.local(2009, "Nov", 28, 21, 15)
   s.opponent = "Sagmählfäger"
   s.rink_id = 2
   s.season_id = 1
 end
 
 Game.seed(:date, :opponent) do |s| 
-  s.date = Time.parse("2009/11/29 - 08:00")
+  s.date = Time.local(2009, "Nov", 29, 8)
   s.opponent = "Sagmählfäger"
   s.rink_id = 1
   s.season_id = 1
