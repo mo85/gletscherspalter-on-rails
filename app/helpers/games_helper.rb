da74e9c206afbbeaf2dbe1472e4fb7e4746d8c00
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-01-25 18:11:58 +0100

    graphics & summary of a game

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,3 +1,14 @@
 module GamesHelper
   
+  def summary_sentence(all_players)
+    goalies = all_players.select{ |p| p.position == "G" }.size
+    result_string = "Total: #{all_players.size - goalies} Spieler und "
+    if goalies == 0
+      result_string << "<strong>kein</strong>"
+    else
+      result_string << "#{goalies}"
+    end
+    result_string << " Goalie"
+    result_string
+  end
 end
