1b7fed64b700dbe4895b369b784d692cc6e34520
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-03-09 22:52:55 +0100

    bugfix?

--- app/controllers/user_pictures_controller.rb
+++ app/controllers/user_pictures_controller.rb
@@ -9,24 +9,27 @@ class UserPicturesController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:user_id])
-    # destroy the old one...
-    @user.user_picture.destroy if @user.user_picture
+    if params[:user_pic]
+      @user = User.find(params[:user_id])
+      # destroy the old one...
+      @user.user_picture.destroy if @user.user_picture
 
-    pic = UserPicture.new params[:uploaded_data]
-    pic.user = @user
+      pic = UserPicture.new params[:user_pic]
+      pic.user = @user
 
-    if pic.save()
-      flash[:notice] = "Das Bild wurde erfolgreich gespeichert."
-      respond_to do |format|
-        format.html { redirect_to player_path(@user.player) }
-      end
-    else
-      flash[:notice] = "Das Bild konnte nicht gespeichert werden."
-      respond_to do |format|
-        format.html { redirect_to player_path(@user.player) }
+      if pic.save()
+        flash[:notice] = "Das Bild wurde erfolgreich gespeichert."
+        respond_to do |format|
+          format.html { redirect_to player_path(@user.player) }
+        end
+      else
+        flash[:notice] = "Das Bild konnte nicht gespeichert werden."
+        respond_to do |format|
+          format.html { redirect_to player_path(@user.player) }
+        end
       end
     end
+
 
   end
 
