b96c68674f1f9f707131a833a9b2252585061ea3
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-03-09 22:20:06 +0100

    user profile pics

--- app/controllers/user_pictures_controller.rb
+++ app/controllers/user_pictures_controller.rb
@@ -10,23 +10,21 @@ class UserPicturesController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-
     # destroy the old one...
     @user.user_picture.destroy if @user.user_picture
 
-    pic = UserPicture.create(params[:uploaded_data])
+    pic = UserPicture.new params[:uploaded_data]
     pic.user = @user
-
 
     if pic.save()
       flash[:notice] = "Das Bild wurde erfolgreich gespeichert."
       respond_to do |format|
-        format.html { redirect_to players_path(@user.player) }
+        format.html { redirect_to player_path(@user.player) }
       end
     else
       flash[:notice] = "Das Bild konnte nicht gespeichert werden."
       respond_to do |format|
-        format.html { redirect_to players_path(@user.player) }
+        format.html { redirect_to player_path(@user.player) }
       end
     end
 
