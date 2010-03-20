be9cd6deadb0b984cc9db1884ba972c5da0db555
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-03-20 17:07:16 +0100

    ajax pagination with prototype

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,7 +1,15 @@
-$(function() {
-	$('.pagination a').live("click", function() {
-		$(".pagination").html("Seite wird geladen...");
-		$.get(this.href, null, null, "script");
-		return false;
-	});
-});
+Glacier.Paginator = {
+	
+	addAjaxPagination: function(containerId) {
+		var elems = $$("div.pagination a");
+
+		elems.each(function(elem) {
+			elem.onclick = function() { return false; }
+			elem.observe("click", function() {
+				$$("div.pagination")[0].update("Seite wird geladen...");
+				new Ajax.Updater(containerId, elem.href, { method: 'get', evalScripts: true});
+			});
+		});
+	}
+		
+};
