5da1064bce9a7c7e979d598b7a3c005425f7ca6b
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-08-15 03:22:55 +0200

    send a mail when new news are created

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -11,5 +11,20 @@ class UserMailer < ActionMailer::Base
     body          "#{message.text}"
     content_type  "text/html"
   end
+  
+  def new_news(creator, message)
+    users = User.all.select{|u| u.email != nil && u.email != ""}
+    
+    if RAILS_ENV == "production"
+      bcc users.collect{|u| "#{u.name} <#{u.email}>"}
+    else
+      bcc users.collect{|u| "#{u.name} <mark_odermatt@bluewin.ch>"}
+    end
+    
+    from          "no-reply@gletscherspalter.ch"
+    subject       "Neuigkeiten auf Gletscherspalter.ch"
+    body          :creator => creator, :message => message
+    content_type  "text/html"
+  end
 
 end
