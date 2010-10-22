503c50d72429f946615065cfbe9906d3a8a3253d
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-10-22 18:30:58 +0200

    fix

--- app/models/season.rb
+++ app/models/season.rb
@@ -37,7 +37,7 @@ class Season < ActiveRecord::Base
   end
   
   def games_played
-    @games_played ||= games.where("score = ? AND opponent_score = ?", "", "")
+    @games_played ||= games.where("score != ? AND opponent_score != ?", "", "")
   end
   
   def wins
