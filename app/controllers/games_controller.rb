3514428ca4ff14f4ad45654c7777a01c2f44949e
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-10-26 18:42:06 +0200

    bugfix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,6 +14,7 @@ class GamesController < ApplicationController
         @games = @season.games.paginate :page => params[:page], :per_page => 10
         
         format.html
+        format.js
         format.pdf do
           @events = @season.games
           render :file => "/players/events.pdf.prawn", :locals => { :events => @events } 
