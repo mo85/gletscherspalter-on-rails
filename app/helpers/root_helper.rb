59b14fc7ed2057797a2e7e02dd90975f3916f708
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-11-09 21:47:47 +0100

    lightbox for history page

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -6,4 +6,13 @@ module RootHelper
     Base64.encode64(address)
   end
   
+  def lightbox_history_img(filename, options)
+    options[:class] ||= "nil"
+    options[:filetype] ||= "jpg"
+    
+    html = "<a href='/images/history/#{filename}-middle.#{options[:filetype]}' rel='lightbox[roadtrip]'>"
+    html += image_tag("history/#{filename}-small.#{options[:filetype]}", :class => "#{options[:class]}")
+    html += "</a>"
+  end
+  
 end
