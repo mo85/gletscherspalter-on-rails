a804bd50f11f1b7c48f5fe9bfad20a5a6253d881
Mark Odermatt <mark_odermatt@bluewin.ch>
2011-01-17 20:18:04 +0100

    captcha for guestbook

--- app/controllers/guestnotes_controller.rb
+++ app/controllers/guestnotes_controller.rb
@@ -2,6 +2,8 @@
 class GuestnotesController < ApplicationController
   filter_access_to :all
   
+  validates_captcha
+  
   def index
     @notes = Guestnote.all.paginate :page => params[:page], :per_page => 10
     respond_to do |format|
@@ -12,11 +14,7 @@ class GuestnotesController < ApplicationController
   
   def new
     @note = Guestnote.new
-    
-    @random_no1 = rand(50)
-    @random_no2 = rand(50)
-    @token = generate_token(@random_no1 + @random_no2)
-    
+
     respond_to do |format|
       format.ajax
     end
@@ -46,16 +44,13 @@ class GuestnotesController < ApplicationController
   
   def create
     @note = Guestnote.new(params[:guestnote])
-    @token = params[:token]
     
-    token_valid = @note.validate_token(@token, generate_token(params[:result].to_i))
-
     respond_to do |format|
-      if token_valid && @note.save
+      if captcha_validated? && @note.save
         flash[:notice] = "Eintrag erfolgreich erstellt."
         format.html { redirect_to guestnotes_path }
       else
-        flash[:notice] = "Eintrag konnte nicht gespeichert werden. Die Angaben waren entweder unvollständig oder falsch."
+        flash[:notice] = "Der Eintrag konnte nicht gespeichert werden da die Sicherheitsprüfung negativ ausfiel."
         format.html { redirect_to guestnotes_path }
       end
     end
@@ -70,12 +65,6 @@ class GuestnotesController < ApplicationController
       format.html { redirect_to guestnotes_path }
     end
     
-  end
-  
-  private
-  
-  def generate_token(number)
-    Digest::SHA1.hexdigest("#{number.to_s} gletscherspalter.ch - goldies #{((0-number) * 5).to_s} - eat my shorts!")
   end
   
 end
