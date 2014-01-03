634357d61cad50551adb849ac627f2739b48c175
Mark Odermatt <mark_odermatt@bluewin.ch>
2014-01-03 09:26:05 +0100

    switching to paperclip

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -2,6 +2,10 @@ class Avatar < ActiveRecord::Base
   
   belongs_to :user
   
-  has_attached_file :photo, :styles => { :medium => "200x400>", :thumb => "50x50>" }
+  has_attached_file :photo, :styles => { :medium => "200x400>", :thumb => "50x50>" },
+  :storage => :s3,
+  :path => "assets/:class/:id/:style/:filename",
+  :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
+  :bucket => "gletscherspalter_on_rails_#{Rails.env}"
   
 end
